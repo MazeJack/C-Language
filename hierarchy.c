c6e0f846e35c195cc7339434c31d2e6d957f309d
@The Jack <abhishekdumaniya196@gmail.com>
2023-08-15 15:55:52 +0530

    hierarchy

---
+++ hierarchy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    // int i;
+    // i=2*3/4+4/4+8-2+5/8;
+    // printf("%d",i);
+    // return 0;
+
+    float j;
+    j=3/2*4+3/8;
+    printf("%f",j);
+    return 0;
+}
