0650597405d92fe2a1243d4a3134c697184adf37
@The Jack <abhishekdumaniya196@gmail.com>
2023-08-19 02:44:25 +0530

    Calloc

---
+++ Dyanamic_Memory_calloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>  // malloc,calloc,realloc,free
+int main()
+{
+    int * ptr;
+    int n;
+    printf("Enter the size of array you want to create: ");
+    scanf("%d",&n);
+    ptr = (int *)calloc(n,sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the value no: %d of this array\n",i);
+        scanf("%d",&ptr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("The value at %d of this array is %d \n",i,ptr[i]);
+       // scanf("%d",&ptr[i]);
+    }
+    
+    return 0;
+}
